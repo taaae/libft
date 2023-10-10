9fe75411ed50c9d2ad019c9a319455924c3bddf1
Timofei Rusanov <trusanov@1-C-9.42heilbronn.de>
2023-10-10 19:57:46 +0200

    lstmap fix (no norminette)

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lstmap_bonus.c                                     :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: trusanov <trusanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:00:48 by trusanov          #+#    #+#             */
-/*   Updated: 2023/10/10 12:58:12 by trusanov         ###   ########.fr       */
+/*   Updated: 2023/10/10 19:53:53 by trusanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,26 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*ret;
 	t_list	*ret_it;
+	t_list	*content_temp;
 
 	if (lst == NULL || f == NULL)
 		return (NULL);
-	ret = ft_lstnew(f(lst->content));
-	lst = lst->next;
+	content_temp = f(lst->content);
+	ret = ft_lstnew(content_temp);
 	if (!ret)
+	{
+		del(content_temp);
 		return (NULL);
+	}
+	lst = lst->next;
 	ret_it = ret;
 	while (lst)
 	{
-		ret_it->next = ft_lstnew(f(lst->content));
-		if (!ret_it)
+		content_temp = f(lst->content);
+		ret_it->next = ft_lstnew(content_temp);
+		if (!ret_it->next)
 		{
+			del(content_temp);
 			ft_lstclear(&ret, del);
 			return (NULL);
 		}
