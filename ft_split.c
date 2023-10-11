af243ca55949b398f08081b3bced96396f80fd14
Timofei Rusanov <trusanov@2-H-7.42heilbronn.de>
2023-10-11 11:37:18 +0200

    ft_split strict fix (no norme)

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,24 @@
 /*   By: trusanov <trusanov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 14:55:06 by trusanov          #+#    #+#             */
-/*   Updated: 2023/10/08 18:59:06 by trusanov         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:33:37 by trusanov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static void	free_arr(char **s, char **s_end)
+{
+	char	**s_it;
+
+	s_it = s;
+	while (s_it != s_end)
+	{
+		free(*s_it);
+		s_it++;
+	}
+	free(s);
+}
 
 static char	*get_line(const char *s, char c)
 {
@@ -26,6 +39,8 @@ static char	*get_line(const char *s, char c)
 		len++;
 	}
 	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
 	res[len] = '\0';
 	index = 0;
 	while (s[index] && s[index] != c)
@@ -59,8 +74,15 @@ static char	**one_word_split(char const *s)
 	int		index;
 
 	ret = (char **)malloc(sizeof(char *) * (2));
+	if (!ret)
+		return (NULL);
 	ret[1] = NULL;
 	ret[0] = (char *)malloc(ft_strlen(s) + 1);
+	if (!ret[0])
+	{
+		free(ret);
+		return (NULL);
+	}
 	ret[0][ft_strlen(s)] = '\0';
 	index = 0;
 	while (s[index])
@@ -92,6 +114,11 @@ char	**ft_split(char const *s, char c)
 		while (*s == c)
 			s++;
 		*ret_it = get_line(s, c);
+		if (!*ret_it)
+		{
+			free_arr(ret, ret_it);
+			return (NULL);
+		}
 		while (*s && *s != c)
 			s++;
 		ret_it++;
